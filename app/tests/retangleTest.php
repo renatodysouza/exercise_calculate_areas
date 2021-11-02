dda3c27923e89d18b03d9abf83f23c8eff1c6a9f
Renato Souza <renatoserra@gmail.com>
2021-11-02 01:10:19 +0000

    change yml config

--- app/tests/retangleTest.php
+++ app/tests/retangleTest.php
@@ -1,5 +1,10 @@
 <?php
 echo file_exists("./src/Retangle.php") . 'teste123';
+echo file_exists("../src/Retangle.php") . 'teste123';
+echo file_exists("../src/Retangle.php") . 'teste123';
+echo dirname(__FILE__) . 'teste';
+echo file_exists(dirname(__FILE__) . "/src/Retangle.php") . 'teste123';
+
 die();
 include "./src/Retangle.php";
 use PHPUnit\Framework\TestCase;
